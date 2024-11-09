31c0f4b1ec1a61a027078b3d12e46bf7544b4683
TylerMcKenna <101288310+TylerMcKenna@users.noreply.github.com>
2024-11-09 16:12:52 -0600

    Did some crappy user authentication, lots of work to be done on logging in

--- music_player/backend/server.js
+++ music_player/backend/server.js
@@ -15,7 +15,7 @@ const bcrypt = require("bcrypt");
 
 
 
-
+// https://www.youtube.com/watch?v=-RCnNyD0L-s
 
 const users = [];
 
@@ -23,14 +23,32 @@ app.get("/users", (req, res) => {
     res.json(users);
 })
 
-app.post("/users", (req, res) => {
-    try{
-        const salt = bcrypt.genSalt()
+app.post("/users", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword } 
+        users.push(user);
+        res.status(201).send();
+    } catch {
+        res.status(500).send();
     }
-    const user = { name: req.body.name, password: req.body.password } 
-    users.push(user);
-    res.status(201).send();
 })
+
+app.post("/users/login", async (req, res) => {
+    const user = users.find(user => user.name == req.body.name)
+    if (user == null) {
+        return res.status(400).send("cannot find user");
+    }
+    try {
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send("Success");
+        } else {
+            res.send("Not allowed");
+        }
+    } catch {
+        res.status(500).send();
+    }
+}) 
 
 app.get('/api/songs', (req, res) => {
     const mysql = require("mysql");
